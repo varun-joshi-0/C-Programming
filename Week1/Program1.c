8257c84832f87e8be4e3685f63e34aa741b4c777
varun-joshi-0 <varunjoshi421@gmail.com>
2025-09-11 21:45:36 +0530

    Create Program1.c

---
+++ Week1/Program1.c
@@ -0,0 +1,10 @@
+//WAP to Print Your Info
+
+#include <stdio.h>
+
+int main() {
+    printf("My Name is Varun Joshi");
+    printf("\nI'm a B.Tech C.S.E student");
+    printf("\nMy SapID is: 590026636");
+    return 0;
+}
