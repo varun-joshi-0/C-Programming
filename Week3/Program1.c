25860581c017b0c38cd95ae98f875f237e71e33f
varun-joshi-0 <varunjoshi421@gmail.com>
2025-09-11 21:07:48 +0530

    Create Program1.c

---
+++ Week3/Program1.c
@@ -0,0 +1,23 @@
+//WAP to check whether a Number is Positive, Negative or Zero
+
+#include <stdio.h>
+int main(){
+    int a  ;
+    printf("Enter a Number: ");
+    scanf("%d",&a);
+    if (a > 0)
+    {
+        printf("The Number is Positive");
+    }
+    if (a < 0)
+    {
+        printf("The Number is Negative");
+    }
+    if (a == 0)
+    {
+        printf("The Number is Zero");
+    }
+return 0;
+
+    
+}
