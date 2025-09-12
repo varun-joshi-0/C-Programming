1e946f3901b46f7152d5c7c7060d2f0706a6a98f
varun-joshi-0 <varunjoshi421@gmail.com>
2025-09-12 09:25:57 +0530

    Create Program1.c

---
+++ Week5/Program1.c
@@ -0,0 +1,17 @@
+//WAP to print the sum of first n natural numbers.
+#include<stdio.h>
+int main(){
+
+    int n, sum;
+    printf("Enter the Limit: ");
+    scanf("%d",&n);
+
+        sum = n * (n + 1) / 2;
+    printf("%d",sum);
+
+
+
+
+
+    return 0 ;
+}
