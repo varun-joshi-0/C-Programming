869a354699049bd3b9051323469151b490bef24d
varun-joshi-0 <varunjoshi421@gmail.com>
2025-09-26 00:29:21 +0530

    Add files via upload

---
+++ 100 Days Of Code/Day01Q1.c
@@ -0,0 +1,17 @@
+// Write a program to input two numbers and display their sum.
+#include <stdio.h>
+int main()
+{
+
+    int a, b, sum;
+    printf("Enter the First Number: ");
+    scanf("%d", &a);
+    printf("Enter the Second Number: ");
+    scanf("%d", &b);
+
+    sum = a + b;
+
+    printf("The Sum of the Numbers are %d", sum);
+
+    return 0;
+}
